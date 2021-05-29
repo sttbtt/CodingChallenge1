82a6e253079552d87bf6fa797f20fcd87f3c0339
Scott Bennett <stt.btt@gmail.com>
2021-05-29 18:12:09 -0400

    Initial Commit

---
+++ CodingChallenge1/CodingChallenge1App.swift
@@ -0,0 +1,17 @@
+//
+//  CodingChallenge1App.swift
+//  CodingChallenge1
+//
+//  Created by Scott Bennett on 5/29/21.
+//
+
+import SwiftUI
+
+@main
+struct CodingChallenge1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodingChallenge1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CodingChallenge1
+//
+//  Created by Scott Bennett on 5/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
